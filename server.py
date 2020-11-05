e49b8f29fcea89b1fba2bb623cbbc2eee41db1ae
Sydney Holden <scholdenak@gmail.com>
2020-11-05 05:11:52 +0000

    Created User class added db connections to server.py and model.py

---
+++ server.py
@@ -0,0 +1,21 @@
+"""Server for covid date generator app."""
+
+from flask import (Flask, render_template, request, flash, session,
+                   redirect)
+from model import connect_to_db
+
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+app.secret_key = "'dflkghnm'[pdftnhmp"
+app.jinja_env.undefined = StrictUndefined
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    connect_to_db(app)
+    app.run(host='0.0.0.0', debug=True)
