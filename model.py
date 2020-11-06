ef18ea86dd5aad93d9f22a5ca9fc68d8a76455e1
Sydney Holden <scholdenak@gmail.com>
2020-11-06 00:01:56 +0000

    defined relationship on date_idea and added __repr__ to Date_idea class

--- model.py
+++ model.py
@@ -36,6 +36,10 @@ class Date_idea(db.Model):
     is_at_home = db.Column(db.Boolean)
     submitted_by = db.Column(db.String, db.ForeignKey('users.user_id'))
 
+    user = db.relationship('User', backref='users')
+
+    def __repr__(self):
+        return f'<Date_idea idea_id={self.idea_id} date_name={self.date_name} submitted_by={self.submitted_by}>'
 
 def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
