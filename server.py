f47a14cfb06792afe8d997a240fb73c6abc1fa0e
Sydney Holden <scholdenak@gmail.com>
2020-11-09 04:58:34 +0000

    created a homepage.html and and app route to homepage

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 """Server for covid date generator app."""
 
 from flask import (Flask, render_template, request, flash, session,
-                   redirect)
+                   redirect,)
 from model import connect_to_db
 
 from jinja2 import StrictUndefined
@@ -10,10 +10,16 @@ app = Flask(__name__)
 app.secret_key = "'dflkghnm'[pdftnhmp"
 app.jinja_env.undefined = StrictUndefined
 
+@app.route('/')
+def homepage():
+    """View homepage"""
 
+    # if user not in session:
+    return render_template('homepage.html')
 
-
-
+@app.route('/login')
+def login():
+    pass
 
 
 if __name__ == '__main__':
