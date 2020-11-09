5f76e81ba36f719453080682dda683f61f7cdc65
Sydney Holden <scholdenak@gmail.com>
2020-11-09 05:14:14 +0000

    added submit button to homepage

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 """Server for covid date generator app."""
 
 from flask import (Flask, render_template, request, flash, session,
-                   redirect,)
+                   redirect, url_for)
 from model import connect_to_db
 
 from jinja2 import StrictUndefined
