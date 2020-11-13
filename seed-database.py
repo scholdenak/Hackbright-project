be2e192d351ed0459353950f298b2f3ced76ca6d
Sydney <scholdenak@gmail.com>
2020-11-13 02:05:51 +0000

    refactored functions in server.py to look cleaner

--- seed-database.py
+++ seed-database.py
@@ -19,13 +19,14 @@ with open('data/date_data.json') as f:
 
 dates_in_db = []
 for date in date_data:
-    date_name, description, is_video, is_socially_distant, is_co_quarantined, is_outside, is_at_home = (date['date_name'],
-                                                                                                        date['description'],
-                                                                                                        date['is_video'],
-                                                                                                        date['is_socially_distant'],
-                                                                                                        date['is_co_quarantined'],
-                                                                                                        date['is_outside'],
-                                                                                                        date['is_at_home'])
+    date_name, description, is_video, is_socially_distant, \
+    is_co_quarantined, is_outside, is_at_home = (date['date_name'],
+                                                 date['description'],
+                                                 date['is_video'],
+                                                 date['is_socially_distant'],
+                                                 date['is_co_quarantined'],
+                                                 date['is_outside'],
+                                                 date['is_at_home'])
     db_date = crud.create_date_idea(date_name, 
                                     description, 
                                     is_video, 
@@ -48,15 +49,3 @@ for user in user_data:
     db_user = crud.create_user(email, password, fname, lname)
     users_in_db.append(db_user)
 
-# # Create 10 users; each user will make 10 ratings
-# for n in range(10):
-#     email = f'user{n}@test.com'  # Voila! A unique email!
-#     password = 'test'
-
-#     user = crud.create_user(email, password)
-
-#     for _ in range(10):
-#         random_movie = choice(movies_in_db)
-#         score = randint(1, 5)
-
-#         crud.create_rating(user, random_movie, score)
