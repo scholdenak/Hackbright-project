031c45ecfa99f9eab1920af6887e33a5c57e9237
Sydney <scholdenak@gmail.com>
2020-11-15 00:16:12 +0000

    created pseudocode for filter_date_ideas

--- crud.py
+++ crud.py
@@ -35,9 +35,57 @@ def create_date_idea(date_name, description, is_video, is_socially_distant, is_c
 
     return date_idea
 
+
+
+def query_date():
+    """queries through date ideas"""
+    pass
+    # DateIdea.query.filter(DateIdea.attribute == bool, 
+                        #   DateIdea.attribute == bool).all()
+
+# Take in two attributes at True => all date ideas with those attributes
+
+# examples:
+# arguments(is_video=True, is_outside=True)
+# output(return all with True is video and True is outside)
+
+# arguments(is_video=True, both=True (is_outside=True or is_at_home=True))
+# output(return all with True is video and either outside or home is true)
+
+# Approach(pseudocode):
+# function takes in the bubble answer and location answer
+def filter_date_ideas(form[bubble], form[location]):
+    """queries date options with arguments of bubble and location from form"""
+
+# q = date idea query start
+    q = DateIdea.query
+# form answer bubble = bubble variable
+    bubble = form[bubble]
+# form answer location = location variable
+    location = form[location]
+# if location variable is both:
+    if location = 'both':
+#   date options = query DateIdeas WHERE ((bubble variable) = True, 
+#                                 home is True or outside is True) all
+        date_options = q.filter(DateIdea.bubble == True, 
+                                (DateIdea.location == True)).all()
+# else:
+    else:
+#   date options = query DateIdeas WHERE ((bubble variable) = True,
+#                                 (location variable) is True) all
+        date_options = q.filter(DateIdea.bubble == True, 
+                                (DateIdea.location == True)).all()
+
+ return date option
+
+# in server.py function
+# function to pick random out of date_options
+
+    
+
 def select_date():
     pass
-    
+
 
 if __name__=='__main__':
     from server import app
