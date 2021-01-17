847d9b62d92810a2886c5ec877a22cb0a0db4f20
Sydney Holden <scholdenak@gmail.com>
2021-01-17 01:25:34 +0000

    added js search box to map

--- static/js/map.js
+++ static/js/map.js
@@ -1,6 +1,6 @@
 let map;
-// let service;
-// let infowindow;
+let service;
+let infowindow;
 
 // const keyWordForm = document.querySelector("#test");
 
@@ -9,142 +9,49 @@ let map;
 // })
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
+  const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 32.2583105, lng: -110.9144216},
     zoom: 12,
     fullscreenControl: false,
   });
-  new google.maps.Marker({
-    position: { lat: 32.25774119157389, lng: -110.83934784940523},
-    map,
-    title: "Golf N' Stuff",
-  });
-  new google.maps.Marker({
-    position: { lat: 32.2442303786999, lng: -110.92449267017642},
-    map,
-    title: "The Loft Drive In Theater",
-  });
-  new google.maps.Marker({
-    position: { lat: 32.31246670237841,  lng: -110.82307031970466},
-    map,
-    title: "Three Canyon Beer and Wine Garden",
-  });
-  new google.maps.Marker({
-    position: { lat: 32.288516274567705, lng:-110.92263469539438},
-    map,
-    title: "Green Things Nursery",
-  });
-  new google.maps.Marker({
-    position: { lat: 32.24137471021612, lng: -110.90622631586236},
-    map,
-    title: "Black Crown Coffee Company",
-  });
-  new google.maps.Marker({
-    position: { lat: 32.253505747332056, lng: -110.89018000686586},
-    map,
-    title: "Firetruck Brewing Company",
-  });
-  new google.maps.Marker({
-    position: { lat: 32.29083694231202, lng: -110.94305229509007
-    },
-    map,
-    title: "Reforma Modern Mexican",
-  });
-}
 
-// function initMap() {
-//   let tucson = new google.maps.LatLng(32.2226, -110.9747);
-// //   infowindow = new google.maps.InfoWindow();
-//   map = new google.maps.Map(document.getElementById("map"), {
-//     center: tucson,
-//     zoom: 13,
-//   });
-// }
-//   let request = {
-//     query: keyWordInput, //need user input
-//     fields: ["name", "geometry"],
-//   };
-  // service = new google.maps.places.PlacesService(map);
-//   service.findPlaceFromQuery(request, (results, status) => {
-//     if (status === google.maps.places.PlacesServiceStatus.OK) {
-//       for (let i = 0; i < results.length; i++) {
-//         createMarker(results[i]);
-//       }
-//       map.setCenter(results[0].geometry.location);
-//     }
-//   });
-// }
-
-// function createMarker(place) {
-//   const marker = new google.maps.Marker({
+  const input = document.getElementById("pac-input");
+  const searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+//   new google.maps.Marker({
+//     position: { lat: 32.25774119157389, lng: -110.83934784940523},
 //     map,
-//     position: place.geometry.location,
+//     title: "Golf N' Stuff",
 //   });
-//   google.maps.event.addListener(marker, "click", () => {
-//     infowindow.setContent(place.name);
-//     infowindow.open(map);
+//   new google.maps.Marker({
+//     position: { lat: 32.2442303786999, lng: -110.92449267017642},
+//     map,
+//     title: "The Loft Drive In Theater",
 //   });
-// }
-
-// function initAutocomplete() {
-//   const map = new google.maps.Map(document.getElementById("map"), {
-//     center: { lat: 32.2226, lng: -110.9747 },
-//     zoom: 13,
-//     mapTypeId: "roadmap",
+//   new google.maps.Marker({
+//     position: { lat: 32.31246670237841,  lng: -110.82307031970466},
+//     map,
+//     title: "Three Canyon Beer and Wine Garden",
 //   });
-//   // Create the search box and link it to the UI element.
-//   const input = document.getElementById("pac-input");
-//   const searchBox = new google.maps.places.SearchBox(input);
-//   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-//   // Bias the SearchBox results towards current map's viewport.
-//   map.addListener("bounds_changed", () => {
-//     searchBox.setBounds(map.getBounds());
+//   new google.maps.Marker({
+//     position: { lat: 32.288516274567705, lng:-110.92263469539438},
+//     map,
+//     title: "Green Things Nursery",
 //   });
-//   let markers = [];
-//   // Listen for the event fired when the user selects a prediction and retrieve
-//   // more details for that place.
-//   searchBox.addListener("places_changed", () => {
-//     const places = searchBox.getPlaces();
-
-//     if (places.length == 0) {
-//       return;
-//     }
-//     // Clear out the old markers.
-//     markers.forEach((marker) => {
-//       marker.setMap(null);
-//     });
-//     markers = [];
-//     // For each place, get the icon, name and location.
-//     const bounds = new google.maps.LatLngBounds();
-//     places.forEach((place) => {
-//       if (!place.geometry) {
-//         console.log("Returned place contains no geometry");
-//         return;
-//       }
-//       const icon = {
-//         url: place.icon,
-//         size: new google.maps.Size(71, 71),
-//         origin: new google.maps.Point(0, 0),
-//         anchor: new google.maps.Point(17, 34),
-//         scaledSize: new google.maps.Size(25, 25),
-//       };
-//       // Create a marker for each place.
-//       markers.push(
-//         new google.maps.Marker({
-//           map,
-//           icon,
-//           title: place.name,
-//           position: place.geometry.location,
-//         })
-//       );
-
-//       if (place.geometry.viewport) {
-//         // Only geocodes have viewport.
-//         bounds.union(place.geometry.viewport);
-//       } else {
-//         bounds.extend(place.geometry.location);
-//       }
-//     });
-//     map.fitBounds(bounds);
+//   new google.maps.Marker({
+//     position: { lat: 32.24137471021612, lng: -110.90622631586236},
+//     map,
+//     title: "Black Crown Coffee Company",
 //   });
-// }
+//   new google.maps.Marker({
+//     position: { lat: 32.253505747332056, lng: -110.89018000686586},
+//     map,
+//     title: "Firetruck Brewing Company",
+//   });
+//   new google.maps.Marker({
+//     position: { lat: 32.29083694231202, lng: -110.94305229509007
+//     },
+//     map,
+//     title: "Reforma Modern Mexican",
+//   });
+}
