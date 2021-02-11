75cb9cc42f78efdc1b9cbad346c10bedc6929f1a
Sydney Holden <scholdenak@gmail.com>
2021-02-11 23:56:19 +0000

    modified map so places could be searched

--- static/js/map.js
+++ static/js/map.js
@@ -8,12 +8,100 @@
 //   const keyWordInput = document.querySelector('input[id=keyword]')
 // })
 
-function initMap() {
-  let map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 32.2583105, lng: -110.9144216},
-    zoom: 12,
-    fullscreenControl: false,
+function initAutocomplete() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 32.2583105, lng: -110.9144216 },
+    zoom: 13,
+    mapTypeId: "roadmap",
   });
+  // Create the search box and link it to the UI element.
+  const input = document.getElementById("pac-input");
+  const searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener("bounds_changed", () => {
+    searchBox.setBounds(map.getBounds());
+  });
+  let markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+    // Clear out the old markers.
+    markers.forEach((marker) => {
+      marker.setMap(null);
+    });
+    markers = [];
+    // For each place, get the icon, name and location.
+    const bounds = new google.maps.LatLngBounds();
+    places.forEach((place) => {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      const icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25),
+      };
+      // Create a marker for each place.
+      markers.push(
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location,
+        })
+      );
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
+
+// function initMap() {
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: 32.2583105, lng: -110.9144216},
+//     zoom: 12,
+//     fullscreenControl: false,
+//   });
+//   const request = {
+//     query: "Sam Hughes",
+//     fields: ["name", "geometry"],
+//   };
+//   service = new google.maps.places.PlacesService(map);
+//   service.findPlaceFromQuery(request, (results, status) => {
+//     if (status === google.maps.places.PlacesServiceStatus.OK) { 
+//       for (let i = 0; i < results.length; i++) {
+//         createMarker(results[i]);
+//       }
+//       map.setCenter(results[0].geometry.location);
+//     }
+//   });
+// }
+
+// function createMarker(place) {
+//   const marker = new google.maps.marker({
+//     map,
+//     position: place.geometry.location,
+//   });
+//   google.maps.event.addListener(marker, "click", () => {
+//     infowindow.setContent(place.name);
+//     infowindow.open(map);
+//   });
+// }
 
   // const input = document.getElementById("pac-input");
   // const searchBox = new google.maps.places.SearchBox(input);
@@ -55,4 +143,4 @@ function initMap() {
 //     map,
 //     title: "Reforma Modern Mexican",
 //   });
-}
+// }
